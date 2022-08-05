6ac475d70557d17f513f5cb7e64faddf0b8e5113
pablobriceno <bricenocordovapablo@gmail.com>
2022-08-05 09:44:15 -0400

    Example content

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>Mi primera página en GitHub Pages 😊</h1>
+    <img
+      src="https://cdn.dribbble.com/users/420183/screenshots/2875637/octocat_github.gif"
+      alt=""
+    />
+  </div>
 </template>
 
 <script>
@@ -7,3 +13,21 @@ export default {
   name: "HomeView",
 };
 </script>
+
+<style lang="scss" scoped>
+.home {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-inline: 12px;
+  width: 100%;
+  text-align: center;
+
+  img {
+    width: 100%;
+    max-width: 500px;
+  }
+}
+</style>
